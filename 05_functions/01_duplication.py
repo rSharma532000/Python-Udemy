b6c154dbc916eaa00cbcb66d8f1477d3f66fe6a9
“RSharma532000” <rsharma000235@gmail.com>
2026-04-27 19:08:18 +0530

    27.04.2026

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,6 @@
+def print_order (name, chai_type):
+    print(f"{name} ordered {chai_type} chai!")
+
+print_order("Aman", "masala")
+print_order("Ritik", "Ginger")
+print_order("Laila", "tulsi")
---
+++ 05_functions/02_complex.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+    print("Fetching the sales data")
+
+def filter_valid_sales():
+    print("Filtering Valid sales")
+
+def summarize_data():
+    print("Summarizing the Sales data")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+    print("Report is Ready")
+
+generate_report()
