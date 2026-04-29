b1cf969d91d3c37e7cf7621f95336142845f53b3
“RSharma532000” <rsharma000235@gmail.com>
2026-04-29 18:38:31 +0530

    29.04.2026

---
+++ 05_functions/03_hiding.py
@@ -0,0 +1,16 @@
+def get_input():
+    print("Getting User input")
+
+def validate_input():
+    print("Validating Input")
+
+def save_to_db():
+    print("Saving to Database")
+
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("User registration complete")
+
+register_user()
