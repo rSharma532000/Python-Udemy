60baede7605564815096e494a4cb865f1212763b
“RSharma532000” <rsharma000235@gmail.com>
2026-05-04 17:32:11 +0530

    04-05-2026

---
+++ 05_functions/09_input_params.py
@@ -0,0 +1,36 @@
+#chai = "Ginger Chai"
+
+#def Prepare_chai(order):
+#    print("preparing", order)
+
+#repare_chai(chai)
+#print(chai)
+
+# chai=[1, 2, 3]
+
+# def edit_chai(cup):
+#     cup[1]=45
+# edit_chai(chai)
+# print(chai)
+
+# edit_chai(chai)
+# print(chai)
+
+# def make_chai(tea, milk,sugar):
+#     print(tea, milk, sugar)
+
+# make_chai("Darjeeling", "No", "low")
+# make_chai(tea="Green", sugar="Medium", milk="No")
+
+# def special_chai(*ingredients, **extras):
+#     print("Ingredients", ingredients)
+#     print("Extras", extras)
+# special_chai("Cinamon", "Cardamom" , sweetner="Honey", foam="Yes")
+
+
+def chai_order(order= None):
+    if order is None:
+        order = []
+    print(order)
+chai_order()
+chai_order()
